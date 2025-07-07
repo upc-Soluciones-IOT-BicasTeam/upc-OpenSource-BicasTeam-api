9b41f9cbdbcbb12ab7314cdfe5fae65e89ff06e7
MiguelHuarcayaChavez <u202116207@upc.edu.pe>
2025-07-07 02:17:01 -0500

    fix: authotize buttom in swagger

--- src/main/java/com/bicasteam/movigestion/api/iam/interfaces/rest/AuthenticationController.java
+++ src/main/java/com/bicasteam/movigestion/api/iam/interfaces/rest/AuthenticationController.java
@@ -4,6 +4,7 @@ import com.bicasteam.movigestion.api.iam.domain.services.UserCommandService;
 import com.bicasteam.movigestion.api.iam.interfaces.rest.resources.AuthResource;
 import com.bicasteam.movigestion.api.iam.interfaces.rest.resources.SignInResource;
 import com.bicasteam.movigestion.api.iam.interfaces.rest.resources.SignUpResource;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,10 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private final UserCommandService userSvc;
     public AuthenticationController(UserCommandService userSvc) { this.userSvc = userSvc; }
+    @SecurityRequirement(name = "bearerAuth")
     @PostMapping("/sign-up") public ResponseEntity<Long> signUp(@RequestBody SignUpResource r) {
         return userSvc.signUp(r.toCommand()).map(id -> ResponseEntity.status(HttpStatus.CREATED).body(id))
                 .orElse(ResponseEntity.badRequest().build());
     }
+    @SecurityRequirement(name = "bearerAuth")
     @PostMapping("/sign-in") public ResponseEntity<AuthResource> signIn(@RequestBody SignInResource r) {
         return userSvc.signIn(r.toCommand()).map(tok->ResponseEntity.ok(new AuthResource(tok)))
                 .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
